5b5c61c1d837f17a538ade7821f1ca69c8ff37ee
Geoff Golder <geoffgolder@gmail.com>
2019-03-09 11:35:30 -0500

    Updates

---
+++ precache-manifest.0539c0ee8425fcdf3eccfc123be83079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "259374c6dfbc4a87f7ae5263f1547109",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/media/addcircle.259374c6.png"
+  },
+  {
+    "revision": "cc35f4ce1ed85b07c64d",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/runtime~main.96935697.js"
+  },
+  {
+    "revision": "b24b61a3f53a5714f26f",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/main.d604733a.chunk.js"
+  },
+  {
+    "revision": "43dc486c09d948fbd577",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/2.99c6db30.chunk.js"
+  },
+  {
+    "revision": "b24b61a3f53a5714f26f",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/css/main.d924a8fd.chunk.css"
+  },
+  {
+    "revision": "50a8ba45bb3c119ce53a806ba15cc381",
+    "url": "/fastai-vision-react-model-inference-ghpages/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fastai-vision-react-model-inference-ghpages/precache-manifest.0539c0ee8425fcdf3eccfc123be83079.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fastai-vision-react-model-inference-ghpages/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
