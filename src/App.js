eb0502633e09a9022c625d9c849e38237953860b
Geoff Golder <geoffgolder@gmail.com>
2019-03-11 16:02:15 -0500

    add ability to upload image

--- src/App.js
+++ src/App.js
@@ -2,105 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import axios from 'axios';
 
 import addcircle from './addcircle.png';
 
 
 import { name as app_name } from '../package.json';
 
+import ClassifyUrl from './ClassifyUrl'
 
-import LoadingOverlay from 'react-loading-overlay';
+
 console.log("Name is ", app_name);
 
-
-class ClassifyUrl extends Component {
-  constructor(props){
-    super(props);
-    this.state = { 
-      overlayActive: false,
-      overlaySpinnerActive: false,
-      overlayText:"Loading..",
-      getApiUrl: props.getApiUrl || 'https://dsm.just-minimalism.com/classify-url',
-      classifyImg: props.classifyImg,
-      enableClassify: true,
-    }
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClassify = this.handleClassify.bind(this);
-    this.toggleOverlay = this.toggleOverlay.bind(this);
-  }
-
-  exampleMapping(data){
-    console.log("inside exampleMapping-->", data,data);
-    console.log("<--inside exampleMapping");
-    const dict = {
-      animal: "This is an animal mouse!",
-      computer: "This is a computer mouse!",
-      micky: "This is micky mouse (and disney just made a dollar).",
-    }
-    return dict[data.predictions[0][0]];
-  }
-
-  handleClick(){
-    this.toggleOverlay();
-  }
-
-  toggleOverlay(){
-    this.setState(state => ({
-        overlayActive: !this.state.overlayActive
-      })
-    );
-  }
-
-  toggleSpinner(){
-    this.setState(state => ({
-        overlaySpinnerActive: !this.state.overlaySpinnerActive
-      })
-    );
-  }
-
-  
-
-  handleClassify(){
-    const base = 'https://dsm.just-minimalism.com/classify-url';
-    const params = '?url=' + encodeURIComponent(this.state.classifyImg);
-    this.toggleOverlay();
-    this.toggleSpinner();
-    this.setState({ overlayText: 'Loading..'});
-    console.log("in handle classify");
-    axios.get(base + params)
-      .then((response) => {
-          console.log(response);
-          this.setState({ 
-            overlayText: this.exampleMapping(response.data),
-            enableClassify: false
-          });
-          this.toggleSpinner();
-        }
-      )
-  }
-
-  render() {
-    return (
-      <LoadingOverlay
-        active={this.state.overlayActive}
-        spinner={this.state.overlaySpinnerActive}
-        text={this.state.overlayText}
-        onClick={this.handleClick}
-      >
-        <div className="App-header">
-          <img src={this.props.classifyImg} className="App-logo" alt=""/>
-          <Link className="App-button-link" to="/classify" >
-            <button className="App-button" onClick={this.handleClassify} disabled={!this.state.enableClassify}>
-              Classify..
-            </button>
-          </Link>
-          <Link to="/" className="App-back-link">Back</Link>
-        </div>
-      </LoadingOverlay>
-    )
-  }
-}
 
 class VerifyUrl extends Component {
   constructor(props){
@@ -129,7 +41,37 @@ class VerifyUrl extends Component {
   }
 }
 
-function Home(props) {
+class Home extends Component {
+
+    constructor(props){
+      super(props);
+      this.imgId = 'ah3bd9-inputfile';
+      this.onChange = this.onChange.bind(this);
+    }
+
+  onChange(){
+    const file   = document.getElementById(this.imgId).files[0];
+
+    const reader = new FileReader();
+
+    const that = this;
+    reader.onloadend = function(){
+      that.props.bubble('verifyImg', reader.result);
+      that.props.bubble('uploaded', true);
+      that.props.bubble('imgBlob', file);
+      that.props.history.push('/classify');
+
+    }
+
+    if (file){
+      reader.readAsDataURL(file);
+    }
+
+  }
+
+
+
+  render(){
     return (
       <div className="App">
         <header className="App-header">
@@ -142,18 +84,19 @@ function Home(props) {
           <p>
             Or
           </p>
-          <Link
-            className="App-link"
-            to="https://reactjs.org"
-          >
-            Take Photo / Upload Photo
-          </Link>
+          <div id="input-hider">
+            <input type="file" onChange={this.onChange} id={this.imgId} accept="image/*"/>
+          </div>
+          <div className="App-link" onClick={()=>document.getElementById(this.imgId).click()}>Upload file / Take photo</div>
         </header>
       </div>
     );
+  }
+
 }
 
 class App extends Component {
+
   constructor(props){
     super(props);
     this.state = { 
@@ -161,15 +104,22 @@ class App extends Component {
       verifyImgUrl: null,
       verifyImg: addcircle,
       validImg: false,
+      uploaded: false,
+      imgBlob: null,
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleError = this.handleError.bind(this);
+    this.bubble = this.bubble.bind(this);
+  }
+
+  bubble(name, val) {
+    console.log("calling set state in bubble with", name, val);
+    this.setState(state => (state[name]=val));
   }
 
 
   handleError(event) {
-    console.log("IN ERROR WITH EVENT", event.target);
     this.setState(state => ({
       verifyImg: addcircle
     }))
@@ -182,11 +132,13 @@ class App extends Component {
       verifyImg: val,
     }))
   }
-  render() {
+  render(props) {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <div className="app-container">
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact render={ (props) => 
+            <Home {...props} bubble={this.bubble}></Home> 
+          } />
           <Route path="/verify-url" exact render={(props) => 
               <VerifyUrl 
                 classifyImg={this.state.classifyImg}
@@ -198,8 +150,11 @@ class App extends Component {
           }/>
           <Route path="/classify" exact render={(props) => 
               <ClassifyUrl 
+                {...props}
                 classifyImg={this.state.verifyImg}
                 handleSubmit={this.handleSubmit} 
+                uploaded={this.state.uploaded}
+                imgBlob={this.state.imgBlob}
               />
           }/>
         </div>
---
+++ src/ClassifyUrl.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import axios from 'axios';
+import LoadingOverlay from 'react-loading-overlay';
+
+class ClassifyUrl extends Component {
+  constructor(props){
+    console.log("ccc props", props);
+    super(props);
+    this.state = { 
+      overlayActive: false,
+      overlaySpinnerActive: false,
+      overlayText:"Loading..",
+      getApiUrl: props.getApiUrl || 'https://dsm.just-minimalism.com/classify-url',
+      classifyImg: props.classifyImg,
+      enableClassify: true,
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClassify = this.handleClassify.bind(this);
+    this.toggleOverlay = this.toggleOverlay.bind(this);
+    this.doPostCallback = this.doPostCallback.bind(this);
+    this.doGetCallback = this.doGetCallback.bind(this);
+  }
+
+  exampleMapping(data){
+    console.log("inside exampleMapping-->", data,data);
+    console.log("<--inside exampleMapping");
+    const dict = {
+      animal: "This is an animal mouse!",
+      computer: "This is a computer mouse!",
+      micky: "This is micky mouse (and disney just made a dollar).",
+    }
+    return dict[data.predictions[0][0]];
+  }
+
+  handleClick(){
+    this.toggleOverlay();
+  }
+
+  toggleOverlay(){
+    this.setState(state => ({
+        overlayActive: !this.state.overlayActive
+      })
+    );
+  }
+
+  toggleSpinner(){
+    this.setState(state => ({
+        overlaySpinnerActive: !this.state.overlaySpinnerActive
+      })
+    );
+  }
+
+  handleClassify(){
+    this.toggleOverlay();
+    this.toggleSpinner();
+    this.setState({ overlayText: 'Loading..'});
+
+    if(this.props.uploaded){
+      this.doPostCallback();
+    } else {
+      this.doGetCallback();
+    }
+	}
+
+ 	doPostCallback(){
+    const base = 'https://dsm.just-minimalism.com/classify-url';
+			fetch(base, {
+			  method: "POST",
+			  body: this.props.imgBlob
+			}).then(res => {
+        console.log("inside the yes", res);
+				return res.json().then(obj=> {
+          console.log("inside the object parse", obj);
+          this.setState({ 
+            overlayText: this.exampleMapping(obj),
+            enableClassify: false
+          });
+          this.toggleSpinner();
+				})
+      }).catch(error => {
+        console.log("THEE CATCH", error);
+          this.setState({ 
+            overlayText: "failed",
+            enableClassify: true
+          });
+          this.toggleSpinner();
+      });
+		}
+  
+
+  doGetCallback(){
+    const base = 'https://dsm.just-minimalism.com/classify-url';
+    //const base = 'http://138.197.227.42/classify-url';
+    const params = '?url=' + encodeURIComponent(this.state.classifyImg);
+    axios.get(base + params)
+      .then((response) => {
+          console.log(response);
+          this.setState({ 
+            overlayText: this.exampleMapping(response.data),
+            enableClassify: false
+          });
+          this.toggleSpinner();
+        }
+      )
+  }
+
+  render() {
+    return (
+      <LoadingOverlay
+        active={this.state.overlayActive}
+        spinner={this.state.overlaySpinnerActive}
+        text={this.state.overlayText}
+        onClick={this.handleClick}
+      >
+        <div className="App-header">
+          <img src={this.props.classifyImg} className="App-logo" alt=""/>
+          <Link className="App-button-link" to="/classify" >
+            <button className="App-button" onClick={this.handleClassify} disabled={!this.state.enableClassify}>
+              Classify..
+            </button>
+          </Link>
+          <Link to="/" className="App-back-link">Back</Link>
+        </div>
+      </LoadingOverlay>
+    )
+  }
+}
+
+export default ClassifyUrl
