9689f85714d9b360fbbe0b81b2f49f18e4532ced
Geoff Golder <geoffgolder@gmail.com>
2019-03-12 10:57:35 -0500

    add waiting indicator while large images are processed

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import LoadingOverlay from 'react-loading-overlay';
+
 class Home extends Component {
 
     constructor(props){
       super(props);
+
+      this.state = {
+        overlayActive: false,
+        overlaySpinnerActive: false,
+        overlayText:"Loading..",
+      }
+
       this.imgId = 'ah3bd9-inputfile';
       this.onChange = this.onChange.bind(this);
     }
 
   onChange(){
-    const file   = document.getElementById(this.imgId).files[0];
+    this.setState({
+        overlayActive: true,
+        overlaySpinnerActive: true,
+        overlayText:"Accessing Image...",
+    })
 
+    const file   = document.getElementById(this.imgId).files[0];
     const reader = new FileReader();
 
     const that = this;
@@ -20,6 +34,9 @@ class Home extends Component {
       that.props.bubble('uploaded', true);
       that.props.bubble('imgBlob', file);
       that.props.history.push('/classify');
+      that.setState({
+          overlayActive: false,
+      })
 
     }
 
@@ -33,27 +50,34 @@ class Home extends Component {
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-panel">
-            <Link
-              className="App-link"
-              to="/verify-url"
-            >
-              Enter Url
-            </Link>
-          </div>
-            <p>
-              Or
-            </p>
-          <div className="App-panel">
-            <div id="input-hider">
-              <input type="file" onChange={this.onChange} id={this.imgId} accept="image/*"/>
+      <LoadingOverlay
+        active={this.state.overlayActive}
+        spinner={this.state.overlaySpinnerActive}
+        text={this.state.overlayText}
+        onClick={this.handleClick}
+      >
+        <div className="App">
+          <header className="App-header">
+            <div className="App-panel">
+              <Link
+                className="App-link"
+                to="/verify-url"
+              >
+                Enter Url
+              </Link>
             </div>
-            <div className="App-link" onClick={()=>document.getElementById(this.imgId).click()}>Upload file / Take photo</div>
-          </div>
-        </header>
-      </div>
+              <p>
+                Or
+              </p>
+            <div className="App-panel">
+              <div id="input-hider">
+                <input type="file" onChange={this.onChange} id={this.imgId} accept="image/*"/>
+              </div>
+              <div className="App-link" onClick={()=>document.getElementById(this.imgId).click()}>Upload file / Take photo</div>
+            </div>
+          </header>
+        </div>
+      </LoadingOverlay>
     );
   }
 
